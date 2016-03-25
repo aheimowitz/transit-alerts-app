9f0c7b1ebc8f5e973bbcbd94cc79a8d97e6b0b4f
pcguy5239 <aheimow1@binghamton.edu>
2016-03-24 22:36:12 -0400

    Initial Commit

---
+++ Transit AlertsTests/Transit_AlertsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Transit_AlertsTests.swift
+//  Transit AlertsTests
+//
+//  Created by Adam Heimowitz on 3/24/16.
+//  Copyright © 2016 app4. All rights reserved.
+//
+
+import XCTest
+@testable import Transit_Alerts
+
+class Transit_AlertsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
